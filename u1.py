e483e7755ed42a653e634b2b60bab6ed16ce66a6
Belinboi08 <gudmundseriknoel.belin@elev.ga.dbgy.se>
2025-09-03 13:28:39 +0200

    u1

---
+++ u1.py
@@ -0,0 +1,34 @@
+import os
+os.system("cls")
+
+print(5 + 5)
+print(5 - 5)
+print(5 * 2)
+print(8 / 2)
+print(6 ** 3)  #upp höjt med tre 
+print(10 // 4) #Det gör så att den delar 10 med 4 utan decimal tal 
+print(10 % 3)  #Det är 10 modulus 3 
+
+x= input("Vad heter du för något ")
+y= input ("Hur gammal är du ")
+
+print("Hejsan " + x + "du är " + y + "år gammal")
+
+a= float(input("använd det första talet "))
+b= float(input("använd det andra talet "))
+
+produkt = a * b 
+
+print("produkten av", a, "och", b, "'är", produkt)
+
+vikt = float(input("Ange din viktigt i kilogramn: "))
+langd = float(input("Ange din längd i meter:  "))
+ 
+bmi = vikt / (langd ** 2)
+
+print("Ditt bmi är:", round(bmi, 2)) 
+
+years = int(input("Hur gammal är du? "))
+Weeks = years * 52
+
+print ("Du har levt ungefär så här många", Weeks, "veckor.")
